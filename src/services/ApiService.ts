480ace23403016697d045d5da0d0ee85844da23c
Thamindu Sathruwan <thaminduj.19@cse.mrt.ac.lk>
2024-05-09 19:48:42 +0530

    Update ApiService.ts

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const BASE_URL = 'https://6spblxvyu4.execute-api.us-east-1.amazonaws.com/Prod/';
+const BASE_URL = 'https://774mkaqhf1.execute-api.us-east-1.amazonaws.com/Prod/';
 const API_KEY = 'YOUR_API_KEY';
 
 export const ApiService = axios.create({
